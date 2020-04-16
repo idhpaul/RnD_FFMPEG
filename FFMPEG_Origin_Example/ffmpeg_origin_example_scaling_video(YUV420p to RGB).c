8813dab13a7f670c0af2f194d93a2a2fd2f242c7
idhpaul <idhpaul@naver.com>
2020-04-16 16:25:38 +0900

    update swscale source

--- FFMPEG_Origin_Example/ffmpeg_origin_example_scaling_video(YUV420p to RGB).c
+++ FFMPEG_Origin_Example/ffmpeg_origin_example_scaling_video(YUV420p to RGB).c
@@ -38,12 +38,10 @@ static void fill_yuv_image(uint8_t* data[4], int linesize[4],
     int width, int height, int frame_index)
 {
     int x, y;
-
     /* Y */
     for (y = 0; y < height; y++)
         for (x = 0; x < width; x++)
             data[0][y * linesize[0] + x] = x + y + frame_index * 3;
-
     /* Cb and Cr */
     for (y = 0; y < height / 2; y++) {
         for (x = 0; x < width / 2; x++) {
@@ -52,20 +50,18 @@ static void fill_yuv_image(uint8_t* data[4], int linesize[4],
         }
     }
 }
-
 int main(int argc, char** argv)
 {
     uint8_t* src_data[4], * dst_data[4];
     int src_linesize[4], dst_linesize[4];
-    int src_w = 320, src_h = 180, dst_w, dst_h;
-    enum AVPixelFormat src_pix_fmt = AV_PIX_FMT_BGRA, dst_pix_fmt = AV_PIX_FMT_NV12;
+    int src_w = 320, src_h = 240, dst_w, dst_h;
+    enum AVPixelFormat src_pix_fmt = AV_PIX_FMT_YUV420P, dst_pix_fmt = AV_PIX_FMT_BGRA/*AV_PIX_FMT_RGB24*/;
     const char* dst_size = NULL;
     const char* dst_filename = NULL;
     FILE* dst_file;
     int dst_bufsize;
     struct SwsContext* sws_ctx;
     int i, ret;
-
     if (argc != 3) {
         fprintf(stderr, "Usage: %s output_file output_size\n"
             "API example program to show how to scale an image with libswscale.\n"
@@ -76,20 +72,17 @@ int main(int argc, char** argv)
     }
     dst_filename = argv[1];
     dst_size = argv[2];
-
     if (av_parse_video_size(&dst_w, &dst_h, dst_size) < 0) {
         fprintf(stderr,
             "Invalid size '%s', must be in the form WxH or a valid size abbreviation\n",
             dst_size);
         exit(1);
     }
-
     dst_file = fopen(dst_filename, "wb");
     if (!dst_file) {
         fprintf(stderr, "Could not open destination file %s\n", dst_filename);
         exit(1);
     }
-
     /* create scaling context */
     sws_ctx = sws_getContext(src_w, src_h, src_pix_fmt,
         dst_w, dst_h, dst_pix_fmt,
@@ -103,14 +96,12 @@ int main(int argc, char** argv)
         ret = AVERROR(EINVAL);
         goto end;
     }
-
     /* allocate source and destination image buffers */
     if ((ret = av_image_alloc(src_data, src_linesize,
-        src_w, src_h, src_pix_fmt, 0)) < 0) {
+        src_w, src_h, src_pix_fmt, 16)) < 0) {
         fprintf(stderr, "Could not allocate source image\n");
         goto end;
     }
-
     /* buffer is going to be written to rawvideo file, no alignment */
     if ((ret = av_image_alloc(dst_data, dst_linesize,
         dst_w, dst_h, dst_pix_fmt, 1)) < 0) {
@@ -118,23 +109,20 @@ int main(int argc, char** argv)
         goto end;
     }
     dst_bufsize = ret;
-
     for (i = 0; i < 100; i++) {
         /* generate synthetic video */
         fill_yuv_image(src_data, src_linesize, src_w, src_h, i);
-
         /* convert to destination format */
         sws_scale(sws_ctx, (const uint8_t* const*)src_data,
             src_linesize, 0, src_h, dst_data, dst_linesize);
-
         /* write scaled image to file */
         fwrite(dst_data[0], 1, dst_bufsize, dst_file);
-    }
 
+        printf("Processing : %d\n", i+1);
+    }
     fprintf(stderr, "Scaling succeeded. Play the output file with the command:\n"
         "ffplay -f rawvideo -pix_fmt %s -video_size %dx%d %s\n",
         av_get_pix_fmt_name(dst_pix_fmt), dst_w, dst_h, dst_filename);
-
 end:
     fclose(dst_file);
     av_freep(&src_data[0]);
